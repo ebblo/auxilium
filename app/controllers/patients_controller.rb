7b0983eb043422651acd25c234a485f7061069b7
Loic Ebbing <ebbing.loic@outlook.com>
2020-08-25 18:05:14 +0200

    patient#show

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,15 +1,20 @@
 class PatientsController < ApplicationController
-  def index
-    @patients = Patient.all
-  end
+  # def index
+  #   @patients = Patient.all
+  # end
 
   def show
     @patient = Patient.find(params[:id])
+    @doctor = @patient.doctor
+    @last_consultation = @patient.consultations.where.not(public_report: nil).last
+    @next_consultation = @patient.consultations.where(public_report: nil).first
+    @consultations = @patient.consultations
+    @prescriptions = @last_consultation.consultation_medications
   end
 
   def index
     @doctor = current_user
     @patients = @doctor.patients
   end
-  
+
 end
