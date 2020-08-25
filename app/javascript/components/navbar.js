a256dda909c44eb37641dd9528dd8bbbd0e4d729
sivan.jeremie@gmail.com <sivan.jeremie@gmail.com>
2020-08-25 10:29:14 +0200

    solve conficlit BS4

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -5,8 +5,7 @@ window.onload = function() {
 
   // TweenMax.to('#logo-navbar', 1,{width: 100, height: 100})
   TweenMax.from('#navbar-test', 3 ,{
-    opacity : 0,
-    ease : Power1.easeOut
+    opacity : 0
   })
 }
 }
