64bafd9de66b51465fe4e35bc9bd1297bfb3cf14
sivan.jeremie@gmail.com <sivan.jeremie@gmail.com>
2020-08-31 09:33:12 +0200

    adjust opacity bug

--- app/javascript/components/navbarLink.js
+++ app/javascript/components/navbarLink.js
@@ -2,12 +2,11 @@ import { Tween } from "jquery";
 
 const navbarLink = () => { 
   window.onload = function () {
-  TweenMax.set('body', {opacity:1});
   let tl = new TimelineMax();
 
   // tl.staggerFrom('.stagger-link', 0.2, {autoAlpha:0}, 0.2)
   //tl.staggerFrom('.stagger-link', 1, {opacity:0, ease: Back.easeOut.config(1.7)}, 0.2, "-=0.5")
-  tl.staggerFrom('.stagger-link', 0.5, {y: -50, opacity:0}, 0.5);
+  tl.staggerFrom('.stagger-link', 0.5, {y: -50, opacity:0}, 0.3);
 }
 }
 export { navbarLink };
