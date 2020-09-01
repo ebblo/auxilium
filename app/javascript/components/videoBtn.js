d92de8f56c6bbe11a86d140b59ab1ccbc5a96855
sivan.jeremie@gmail.com <sivan.jeremie@gmail.com>
2020-09-01 11:34:33 +0200

    videocall btn + sidebar show patient info

--- app/javascript/components/videoBtn.js
+++ app/javascript/components/videoBtn.js
@@ -4,15 +4,18 @@ const videoBtn = () => {
   window.onload = function () {
   let tl = new TimelineMax({repeat: 1, yoyo: true});
   
-  tl.from('#video-call', 5, {duration: 2.5, ease: "power1.in", autoAlpha : 0})
+  // tl.from('#video-call', 5, {duration: 2.5, ease: "power1.in", autoAlpha : 0})
 
-  tl.yoyo(true).repeat(100).timeScale(2).play(0.5);
+  // tl.yoyo(true).repeat(100).timeScale(2).play(0.5);
+
+
   // tl.reverse(0);
   // // tl.repeat(10);
   
-  document.getElementById('video-call').addEventListener('load', function(){
-    tl.yoyo();
-  })
+  // document.getElementById('video-call').addEventListener('load', function(){
+  //   tl.yoyo();
+  // })
+  
   // document.getElementById('video-call').addEventListener('load', function(){
   //   tl.reverse();
   // })
