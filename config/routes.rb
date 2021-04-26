c2d356ec691337558b841231ff9d140c11f71bfd
Johann Groll <groll.johann@gmail.com>
2021-04-26 19:33:34 +0200

    doctor's side done, starting patient's side

--- config/routes.rb
+++ config/routes.rb
@@ -6,41 +6,26 @@ Rails.application.routes.draw do
   devise_for :doctors, path: "doctors", controllers: { sessions: "doctors/sessions" }
   devise_for :patients, path: "patients", controllers: { sessions: "patients/sessions" }
 
-  # root page (it's also where you land after sign_out)
   root to: "pages#home"
 
-  # redirection after login
   get '/user', to: "pages#dashboard", :as => :user_root
 
-  # dashboard
   resources :patients, only: [ :index, :show ] do
     resources :consultations, only: [ :index, :create ]
   end
-
   resources :consultations, only: [ :show, :edit, :update ] do
     resources :consultation_medications, only: [ :create, :update ]
   end
-
   resources :consultation_medications, only: [ :destroy ]
-
-  get "/my_profile", to: "patients#my_profile", :as => :my_profile
-
-  # chatroom
   resources :chatrooms, only: [ :show ] do
     resources :messages, only: [:create]
   end
-
   resources :videorooms, only: [ :show, :update ]
 
-  # video call route
-  # get '/calls', to: 'calls#show'
-
-  # errorpages routes
   get "/404", :to => "errors#not_found"
   get "/422", :to => "errors#unacceptable"
   get "/500", :to => "errors#internal_error"
 
-  # configure sidekiq
   require "sidekiq/web"
   authenticate :user, ->(user) { user.admin? } do
     mount Sidekiq::Web => '/sidekiq'
